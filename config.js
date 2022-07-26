414db6465b0a511040820e7ff75e799b90453dda
Bot-waexe <109065685+Bot-waexe@users.noreply.github.com>
2022-07-26 15:27:46 +0200

    Update config.js

--- config.js
+++ config.js
@@ -41,26 +41,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['918270026275','916380260672'] //you can add multiple number
-global.ownername = "𝗧𝚯𝗫𝗜𝗖 𝗧𝗨𝗥𝗕𝚯"
-global.ytname = "YT: Turbo Mods" //ur yt chanel name
-global.socialm = "GitHub: TURBOHYPER" //ur github or insta name
-global.location = "India, TamilNadu, Coimbatore" //ur location
+global.owner = ['14127697165'] //you can add multiple number
+global.ownername = "𝐿𝑈𝐶𝐼𝐼"
+global.ytname = "YT:𝐿𝑈𝐶𝐼𝐼" //ur yt chanel name
+global.socialm = "GitHub:．" //ur github or insta name
+global.location = "𝐼𝑡𝑎𝑙𝑦" //ur location
 
 //bot bombdy
-global.botname = "𝗧𝚯𝗫𝗜𝗖 ₪ 𝚫𝗟𝗘𝗫𝚫" //ur bot name
-global.websitex = "https://github.com/TURBOHYPER" //ur website
+global.botname = "𝐿𝑈𝐶𝐼𝐼 ₪ 𝑇𝐸𝑆𝑇" //ur bot name
+global.websitex = "．" //ur website
 global.vidmenu = fs.readFileSync("./TurboMedia/theme/alexa.mp4") //gif and video menu
-global.packname = "𝗧𝚯𝗫𝗜𝗖 ₪ 𝚫𝗟𝗘𝗫𝚫"
-global.author = "𝗧𝚯𝗫𝗜𝗖 𝗧𝗨𝗥𝗕𝚯"
-global.themeemoji = "💖"
-global.reactmoji = "🌹"
-global.ownertag = ['916380260672'] //ur owner number
-global.ownernummenu = ['916380260672'] //ur owner number in menu and all
-global.watermark = " 𝗧𝚯𝗫𝗜𝗖 𝗧𝗨𝗥𝗕𝚯" //ur watermark
-global.botscript = "https://github.com/TURBOHYPER/Toxic-Alexa_V3"
-global.linkz1 = "https://chat.whatsapp.com/LWjJ4tu2qe9BWQZ1JzRZgp"
-global.linkz2 = "https://chat.whatsapp.com/GsgzyBU8Vld0YCNnFrBAQt"
+global.packname = "𝐿𝑈𝐶𝐼𝐼 ₪ 𝑇𝐸𝑆𝑇"
+global.author = "𝐿𝑈𝐶𝐼𝐼 ₪ "
+global.themeemoji = "💜"
+global.reactmoji = "💜"
+global.ownertag = ['14127697165'] //ur owner number
+global.ownernummenu = ['14127697165'] //ur owner number in menu and all
+global.watermark = " 𝐿𝑈𝐶𝐼𝐼 ₪ 𝑇𝐸𝑆𝑇" //ur watermark
+global.botscript = "."
+global.linkz1 = "https://chat.whatsapp.com/IHMC7Lv7H78Jm5yPkrhSt7"
+global.linkz2 = "."
 
 //Bot theme media
 global.thum = fs.readFileSync("./TurboMedia/theme/alexa.jpg") //ur thumb pic
